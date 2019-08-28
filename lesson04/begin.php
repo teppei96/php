dfd6eb22ebda8f23a505f4dd7ef5889646dddd58
teppei96 <teppeideathnote@gmail.com>
2019-08-29 01:50:57 +0900

    初めてのbegin

---
+++ lesson04/begin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>PHP入門教室</title>
+</head>
+<body>
+<?php
+print('こんにちは,世界!<br />');
+print('こんにちは,PHP!<br />');
+?>
+</body>
+</html>
