d331e288d3033145cd02335f38a80c2719b8758f
teppei96 <teppeideathnote@gmail.com>
2019-09-05 21:12:30 +0900

    second add lesson14

--- lesson14/request1.php
+++ lesson14/request1.php
@@ -1,4 +1,10 @@
-【ここに入力】
+<?php
+$defs =array(
+  'name' => '山田太郎', 'email' => 'yamada@wings.msn.to',
+  'zip' => '100-0000', 'sex' => '男性', 'age' => '40',
+  'os' => array('win','linux'), 'memo' => '特になし'
+);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,35 +14,68 @@
 <body>
 <h3>アンケート</h3>
 <form method="POST" action="request2.php">
+
   <div class="container">
     <label for="name">名前：</label><br />
-【ここに入力】
+    <input type = "text" id="name" name="name" value="<?php print($defs['name']); ?>" />
   </div>
+
   <div class="container">
     <label for="email">メールアドレス：</label><br />
-【ここに入力】
+    <input type= "email" id="email" name="email" value="<?php print($defs['email']);?>" />
   </div>
+
   <div class="container">
     <label for="zip">郵便番号：</label><br />
-【ここに入力】
+    <input type="text" id="zip" name="zip" value="<?php print($defs['zip']);?>" />
   </div>
+
   <div class="container">
     性別：<br />
-【ここに入力】
+    <?php
+    $sexes =array('男性','女性','その他');
+    foreach($sexes as $sex){
+      print('<label>');
+      print('<input type="radio" name="sex" value="'.$sex.'" ');
+      if($sex === $defs['sex']){print('checked');}
+      print('/>');
+      print($sex.'</label>');
+    }
+     ?>
   </div>
+
   <div class="container">
     <label for="age">年齢：</label><br />
     <select id="age" name="age">
-【ここに入力】
+    <?php
+    for($i=10; $i<=50; $i+=10){
+      print('<option value="'.$i.'"');
+      if($i === (int)$defs['age']) {print('selected');}
+      print('>'.$i.'代</option>');
+    }
+     ?>
     </select>
   </div>
+
   <div class="container">
     ご使用のOS：<br />
-【ここに入力】
+    <?php
+    $oss = array('win' => 'Windows', 'mac' => 'Mac', 'linux' => 'Linux');
+    foreach($oss as $k_os => $v_os){
+      print('<label>');
+      print('<input type="checkbox", name="os[]" value="'.$k_os.'" ');
+      foreach($defs['os'] as $os){
+        if($os === $k_os){print('checked');}
+      }
+      print('/>');
+      print($v_os.'</label>');
+    }
+     ?>
   </div>
   <div class="container">
       <label for="memo">サイトへのご意見：</label><br />
-【ここに入力】
+      <textarea rows="5" cols="30" id="memo"
+      name="memo"><?php print($defs['memo']); ?></textarea>
   </div>
   <input type="hidden" name="quest_num" value="XXX15204" />
   <input type="submit" value="送信" />
